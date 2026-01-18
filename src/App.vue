09d1b49aa62795079d6d3aec8c3dd7a25aafc1aa
dusannesic817 <dusannesic28@gmail.com>
2026-01-18 16:38:32 +0100

    guestSidebar

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
 import { ref } from 'vue'
 import Navbar from '@/components/Navbar.vue';
 import Sidebar from '@/components/Sidebar.vue';
+import GuestSidebar from './components/GuestSidebar.vue';
 import { RouterView } from 'vue-router';
 import { useUIStore } from '@/stores/ui'
+import { useAuthStore } from './stores/auth';
 
 const uiStore = useUIStore()
+const authStore = useAuthStore();
 
 </script>
 
 <template>
   <Navbar />
   <div class="flex min-h-screen bg-gray-50">
-    <Sidebar v-show="uiStore.showSidebar" />
+    <GuestSidebar  />
     <main class="flex-1 transition-all duration-300">
       <RouterView />
     </main>
---
+++ src/components/GuestSidebar.vue
@@ -0,0 +1,86 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import { ArchiveBoxIcon } from '@heroicons/vue/24/outline'
+import { ChatBubbleBottomCenterIcon } from '@heroicons/vue/24/outline'
+import { BriefcaseIcon } from '@heroicons/vue/24/outline'
+import { BellAlertIcon } from '@heroicons/vue/24/outline'
+import { HandThumbUpIcon } from '@heroicons/vue/24/outline'
+import { StarIcon } from '@heroicons/vue/24/outline'
+import { UserCircleIcon } from '@heroicons/vue/24/outline'
+import { CurrencyEuroIcon } from '@heroicons/vue/24/outline'
+</script>
+
+<template>
+    <!-- Sidebar -->
+    <aside class="w-48 bg-white shadow-lg p-4 transition-all overflow-hidden">
+        <div class="flex items-center justify-between mb-2 text-gray-700 border-b pb-2 border-gray-300">
+            <!-- Ikonica + ime + email -->
+            <div class="flex items-center">
+                <div class="space-y-1">
+                    <h2 class="text-lg font-semibold">Please Log in</h2>
+                    <p class="text-sm text-gray-500">Ukoliko zelite da objavljujete oglase i licitirate, morate biti prijavljeni</p>
+                </div>
+            </div>                
+        </div>
+        <div class="flex flex-col items-center mb-2 text-gray-700 border-b pb-2 border-gray-300 space-y-2">                                   
+            <RouterLink to="/login"
+                    class="bg-orange-300 text-white w-full text-center py-2 rounded-full hover:bg-orange-400 transition">Login
+            </RouterLink>                                      
+            <RouterLink to="/register"
+                    class="bg-orange-400 text-white w-full text-center py-2 rounded-full hover:bg-orange-500 transition">Register
+            </RouterLink>                                                
+        </div>
+        
+        <ul class="space-y-1 border-b pb-2 border-gray-200 text-sm blur-xs pointer-events-none select-none">
+            <li>
+                <div class="flex items-center gap-3 px-2 py-2 rounded-md bg-sky-100 text-sky-700 font-semibold">
+                    <ArchiveBoxIcon class="w-5 h-5 text-sky-700 text-base" />
+                    <span>My Auctions</span>
+                </div>
+            </li>
+
+            <li>
+                <div class="flex items-center gap-3 px-2 py-2 rounded-md">                        
+                    <BriefcaseIcon class="w-5 h-5 text-sky-700 text-base" />
+                    <span class="text-gray-700 font-medium">My Bids</span>
+                </div>
+            </li>
+
+            <li>
+                <div class="flex items-center gap-3 px-2 py-2 rounded-md">
+                    <ChatBubbleBottomCenterIcon class="w-5 h-5 text-sky-700 text-base" />
+                    <span class="text-gray-700 font-medium">Messages</span>
+                </div>
+            </li>
+            
+            <li>
+                <div class="flex items-center gap-3 px-2 py-2 rounded-md">
+                    <BellAlertIcon class="w-5 h-5 text-sky-700 text-base" />
+                    <span class="text-gray-700 font-medium">Notification</span>
+                </div>
+            </li>
+            
+            <li>
+                <div class="flex items-center gap-3 px-2 py-2 rounded-md">
+                    <HandThumbUpIcon class="w-5 h-5 text-sky-700 text-base" />
+                    <span class="text-gray-700 font-medium">Reviews</span>
+                </div>
+            </li>
+            
+            <li>
+                <div class="flex items-center gap-3 px-2 py-2 rounded-md">
+                    <StarIcon class="w-5 h-5 text-sky-700 text-base" />
+                    <span class="text-gray-700 font-medium">Follow</span>
+                </div>
+            </li>
+        </ul>
+        
+        <!-- ✅ BLUR SEKCIJA -->
+        <div class="border-b pb-2 pt-2 border-gray-200 text-sm blur-xs pointer-events-none select-none">
+            <div class="flex items-center gap-3 px-2 py-2 rounded-md">
+                <UserCircleIcon class="w-5 h-5 text-sky-700 text-base" />
+                <span class="text-gray-700 font-medium">My Profile</span>
+            </div>
+        </div>
+    </aside>        
+</template>
